6c2b831493382338e46ed4dde35904bf142bbcf9
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-04-04 02:23:57 +0530

    Added 24 hours block feature if user attemp login failed 5 times

--- app.js
+++ app.js
@@ -21,21 +21,41 @@ app.post("/login", async (req, res) => {
     const isUserExist = await User.findOne({ email });
     // console.log(isUserExist);
     if (isUserExist) {
-      if (isUserExist.wrong_login_attemps === 5) {
+      if (isUserExist.block_date) {
+        const date = new Date().getTime();
+        const blockDate = isUserExist.block_date.getTime();
+        if (date >= blockDate) {
+          isUserExist.block_date = "";
+          isUserExist.wrong_login_attemps = 0;
+          await isUserExist.save();
+        }
+      }
+      if (isUserExist.wrong_login_attemps > 4) {
+        // if (!isUserExist.block_date) {
+        //   isUserExist.block_date = new Date(new Date().getTime() + 86400000);
+        //   await isUserExist.save();
+        // }
         res.status(400).send({
-          message:
-            "You have reached your maximum login failed attemps try after 24 hours",
+          message: `You have reached your maximum login failed attemps, you can login after ${isUserExist.block_date.toLocaleDateString()}, ${isUserExist.block_date.toLocaleTimeString()}`,
         });
       } else if (isUserExist.password === password) {
         isUserExist.token = token;
+        isUserExist.block_date = "";
+        isUserExist.wrong_login_attemps = 0;
         await isUserExist.save();
         res
           .status(200)
           .send({ message: "User login successful", data: isUserExist });
       } else {
+        isUserExist.block_date = new Date(new Date().getTime() + 86400000);
+        await isUserExist.save();
         isUserExist.wrong_login_attemps += 1;
         await isUserExist.save();
-        res.status(400).send({ message: "Wrong password entered" });
+        res.status(400).send({
+          message: `Wrong password entered, only ${
+            5 - isUserExist.wrong_login_attemps
+          } attemps left`,
+        });
       }
     } else {
       const user = { email, password, token };
