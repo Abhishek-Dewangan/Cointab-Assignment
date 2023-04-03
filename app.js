1dbf2a149fe32839db1e1129df3dce964d6f8a2e
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-04-03 15:39:57 +0530

    Added logout feature in application

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const connection = require('./Database/Connection');
-const User = require('./Models/User');
+const express = require("express");
+const cors = require("cors");
+const connection = require("./Database/Connection");
+const User = require("./Models/User");
 
 const app = express();
 connection();
@@ -9,56 +9,59 @@ connection();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the Software Developer Assignment');
+app.get("/", (req, res) => {
+  res.send("Welcome to the Software Developer Assignment");
 });
 
 // User login request
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
   try {
-    const {email, password} = req.body;
+    const { email, password } = req.body;
     const token = Math.floor(Math.random() * (1000000 - 100000) + 100000);
-    const isUserExist = await User.find({email});
+    const isUserExist = await User.findOne({ email });
+    console.log(isUserExist);
     if (isUserExist) {
-      if (isUserExist.password === req.body.password) {
+      if (isUserExist.password === password) {
         isUserExist.token = token;
         await isUserExist.save();
         res
           .status(200)
-          .send({message: 'User login successful', data: isUserExist});
+          .send({ message: "User login successful", data: isUserExist });
       } else {
-        res.status(400).send({message: 'Wrong password entered'});
+        console.log("wong");
+        res.status(400).send({ message: "Wrong password entered" });
       }
     } else {
-      const user = {email, password, token};
+      const user = { email, password, token };
       const response = await User(user).save();
       res
         .status(200)
-        .send({message: 'User signup and login successful', data: response});
+        .send({ message: "User signup and login successful", data: response });
     }
   } catch (error) {
-    res.status(400).send({message: error.message, error});
+    console.log("error");
+    res.status(400).send({ message: error.message, error });
   }
 });
 
 // User logout request
-app.post('/logout/:userid', async (req, res) => {
+app.post("/logout/:userid", async (req, res) => {
   try {
-    const {userid} = req.params;
-    const {token} = req.body;
-    const user = await User.findOne({_id: userid, token});
+    const { userid } = req.params;
+    const { token } = req.body;
+    const user = await User.findOne({ _id: userid, token });
     if (user) {
-      user.token = '';
+      user.token = "";
       await user.save();
-      res.status(200).send({message: 'Logout successful'});
+      res.status(200).send({ message: "Logout successful" });
     } else {
-      res.status(498).send({message: 'Invalid token'});
+      res.status(498).send({ message: "Invalid token" });
     }
   } catch (error) {
-    res.status(404).send({message: error.message, error});
+    res.status(404).send({ message: error.message, error });
   }
 });
 
 app.listen(8080, () => {
-  console.log('App is runnig on http://localhost:8080');
+  console.log("App is runnig on http://localhost:8080");
 });
