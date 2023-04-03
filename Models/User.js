19c68a65a5b4d13a314e50bd520b0a21f4170df0
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-04-04 01:18:10 +0530

    Block date key is added in schema

--- Models/User.js
+++ Models/User.js
@@ -5,6 +5,7 @@ const UserSchema = mongoose.Schema({
   password: { type: String, required: true },
   token: { type: String },
   wrong_login_attemps: { type: Number, default: 0 },
+  block_date: { type: Date },
 });
 
 module.exports = mongoose.model("user", UserSchema);
